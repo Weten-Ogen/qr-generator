64bd4f5f780a938cd72516aa559737fedf3b9567
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2022-07-03 19:35:01 +0000

    g

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const wrapper = document.querySelector('.wrapper'),
 qrInput = wrapper.querySelector('.form input'),
 generateBtn = wrapper.querySelector('.form button')
-qrImg = wrapper.querySelector('.qr-code');
+qrImg = wrapper.querySelector('.qr-code img');
 
 generateBtn.addEventListener('click' , () =>{
     let qrValue = qrInput.value;
@@ -11,7 +11,7 @@ generateBtn.addEventListener('click' , () =>{
     qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
     qrImg.addEventListener('load', () =>{
          wrapper.classList.add('active');
-         generateBtn.innerText = "Generate QR Code...";
+         generateBtn.innerText = "Generate QR Code";
     });
 
 });
@@ -20,4 +20,4 @@ qrInput.addEventListener('keyup', ()=>{
     if(!qrInput.value){
         wrapper.classList.remove('active')
     }
-})
+});
